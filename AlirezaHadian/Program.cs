5ac0ee19e4c4cd1ab0cc5fdfbb50d44a9a7d70b8
Alireza Hadian <ali.h.reza8@gmail.com>
2024-11-05 01:06:14 +0330

    Changed Style/Add Mockup

--- AlirezaHadian/Program.cs
+++ AlirezaHadian/Program.cs
@@ -11,17 +11,27 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System;
 using System.Globalization;
+using WebMarkupMin.AspNetCore6;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
 builder.Configuration.GetConnectionString("DatabaseConnection")
     ));
-
+builder.Services.AddWebMarkupMin(options =>
+{
+    options.AllowMinificationInDevelopmentEnvironment = true;
+    options.AllowCompressionInDevelopmentEnvironment = true;
+})
+    .AddHtmlMinification()
+    .AddHttpCompression()
+    .AddXmlMinification();
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders().AddDefaultUI()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddHttpClient<ICaptchaValidator, GoogleReCaptchaValidator>();
@@ -46,16 +56,16 @@ builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+//if (!app.Environment.IsDevelopment())
+//{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseWebMarkupMin();
 app.UseRouting();
 
 app.UseAuthentication();
